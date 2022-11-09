38a8f6a783a68b5a7717bcc833a52d3d24676ea0
0008ua <0008.ua@gmail.com>
2022-11-09 07:36:05 +0200

    update mongoose. Change node and npm versions

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,23 +4,17 @@ const log = require('../config/winston')(module);
 
 const options = {
   autoIndex: process.env.NODE_ENV !== 'production', // Mongoose-specific option. Set to false to disable automatic index creation for all models associated with this connection.
-  poolSize: 50, // Maintain up to 50 socket connections
-  wtimeout: 2500,
-  bufferMaxEntries: 0, // If not connected, return errors immediately rather than waiting for reconnect
+  wtimeoutMS: 2500,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true, // Mongoose-specific option. If true, this connection will use createIndex() instead of ensureIndex() for automatic index builds via Model.init()
 };
 
 const url = config.get('DB_URL');
 
-mongoose.connect(url, options)
-  .then(() => log.verbose('Connected to db'),
-    (err) => {
-      log.error('Mongoose Error ' + err);
-      process.exit(1);
-    }
-  );
+mongoose
+  .connect(url, options)
+  .then(() => log.verbose('Connected to db'))
+  .catch((err) => log.error('Mongoose Error ' + err));
 
 // mongoose.connection.on('error', function(err) {
 //   console.log('on error', err);
